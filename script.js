baa8844db1c9ff193ea53d5b7118469d1c894abe
Elisabete Martins de Oliveira <bete.mrt@gmail.com>
2022-08-02 18:22:11 -0300

    Terceiro requisito

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
 let visualizar = document.getElementById('visualizar');
-let escolhas = document.getElementById('color-palette');
-let cores = [], cor;
+const escolhas = document.getElementById('color-palette');
+const cores = [];
+let cor;
 
-function criarcores(){
-    for (let i = 0; i < 4; i += 1){
-      cor = 'rgb('+ (Math.random() * 256)+ ',' + (Math.random() * 256) + ',' + (Math.random() * 256) + ')';
-      cores.push(cor);
+function criarcores() {
+  let random = [];
+  for (let i = 0; i < 4; i += 1) {
+    for(let j = 0; j < 3; j += 1) {
+        random[j] = (Math.random() * 256);
     }
+    cor = 'rgb(' + (random[0]) + ',' + (random[1]) + ',' + (random[2]) + ')';
+    cores.push(cor);
+  }
 }
 
-function criarbotoes(){
-    for (let i = 0; i < 4; i += 1){
-        let button = document.createElement('button'); 
+function criarbotoes() {
+  for (let i = 0; i < 4; i += 1) {
+    let button = document.createElement('button');
+    if (i === 0){
+        button.value = 'black';
+        button.style.backgroundColor = 'black';
+    } else {
         button.value = cores[i];
-        button.type = 'button';
         button.style.backgroundColor = cores[i];
-        button.className = 'color';
-     //   button.addEventListener('click', handler(button));
-        escolhas.appendChild(button);
     }
+    button.type = 'button';
+    button.className = 'color';
+    escolhas.appendChild(button);
+  }
 }
 
 criarcores();
-criarbotoes()
+criarbotoes();
